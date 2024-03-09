fa2f7ac84c1f5778bd23423c8415dc7815543ca1
mlyyppol <apenap1@miumg.edu.gt>
2024-03-09 12:02:27 -0600

    commit 1 primer parcial

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Parcial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("primer parcial de programación")
+        }
+    }
+}
